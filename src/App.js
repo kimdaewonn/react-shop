ecc5a6b083e0cba635c5319f44d653dd2a94849d
kimdaewonn <eodnjs9605@gmail.com>
2023-01-24 23:53:53 +0900

    컴포넌트 데이터 바인딩 + map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "./App.css";
+import data from "./data.js";
 
 function App() {
+    let [shoes, setshoes] = useState(data);
+
     return (
         <div className="App">
             <Navbar bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand id="ddd" href="#home">
-                        Navbar
+                        Shop
                     </Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#features">Cart</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
+            <div className="main-bg"></div>
+            <div className="container">
+                <div className="row">
+                    {/* <Card shoes={shoes[1]} i={2} />
+                    <Card shoes={shoes[2]} i={3} /> */}
+                    {shoes.map((a, i) => {
+                        return <Card shoes={shoes[i]} i={i} />;
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
-
+function Card(props) {
+    return (
+        <div className="col-md-4">
+            <img
+                src={`https://codingapple1.github.io/shop/shoes${
+                    props.i + 1
+                }.jpg`}
+                width={"80%"}
+            />
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.price}</p>
+        </div>
+    );
+}
 export default App;
