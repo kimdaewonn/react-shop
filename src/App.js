e83011c796361fcacf54815a9dcfb6194cc3e5a7
kimdaewonn <eodnjs9605@gmail.com>
2023-01-26 18:10:13 +0900

    useEffecet

--- src/App.js
+++ src/App.js
@@ -1,89 +1,67 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import "./App.css";
 import data from "./data.js";
 import Detail from "./pages/Detail";
 
 function App() {
-    let [shoes] = useState(data);
-    let navigage = useNavigate();
+  let [shoes] = useState(data);
+  let navigage = useNavigate();
 
-    return (
-        <>
-            <div className="App">
-                <Navbar bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand id="ddd" href="#home">
-                            Shop
-                        </Navbar.Brand>
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link
-                                onClick={() => {
-                                    navigage("/detail");
-                                }}
-                                href="#features"
-                            >
-                                Cart
-                            </Nav.Link>
-                        </Nav>
-                    </Container>
-                </Navbar>
+  return (
+    <>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand id="ddd" href="#home">
+              Shop
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link
+                onClick={() => {
+                  navigage("/detail");
+                }}
+                href="#features"
+              >
+                Cart
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
 
-                <div className="main-bg"></div>
-                <div className="container">
-                    <div className="row">
-                        {shoes.map((a, i) => {
-                            return <Card shoes={shoes[i]} i={i} />;
-                        })}
-                    </div>
-                </div>
-            </div>
-            <Routes>
-                <Route path="/" element={<div>메인페이지</div>}></Route>
-                <Route path="/detail" element={<Detail />}></Route>
-                <Route path="*" element={<div>없는페이지에요~</div>}></Route>
-                <Route
-                    path="/about"
-                    element={<div>어바웃페이지임</div>}
-                ></Route>
-                <Route path="/event" element={<Event />}>
-                    <Route
-                        path="one"
-                        element={<div>첫 주문시 양배추즙 서비스</div>}
-                    />
-                    <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
-                </Route>
-            </Routes>
-            <Link to="/">홈</Link>
-            <Link to="/detail">상세페이지</Link>
-        </>
-    );
-}
-
-function Event() {
-    return (
-        <div>
-            <h4>오늘의 이벤트</h4>
-            <Outlet></Outlet>
+        <div className="container">
+          <div className="row">
+            {shoes.map((a, i) => {
+              return <Card shoes={shoes[i]} i={i} />;
+            })}
+          </div>
         </div>
-    );
+      </div>
+      <Routes>
+        <Route path="/" element={<div>메인페이지</div>}></Route>
+        <Route path="/detail" element={<Detail shoes={shoes} />}></Route>
+      </Routes>
+      <Link style={{ paddingRight: "10px", paddingLeft: "650px" }} to="/">
+        홈
+      </Link>
+      <Link to="/detail">상세페이지</Link>
+    </>
+  );
 }
 
 function Card(props) {
-    return (
-        <div className="col-md-4">
-            <img
-                src={`https://codingapple1.github.io/shop/shoes${
-                    props.i + 1
-                }.jpg`}
-                width={"80%"}
-            />
-            <h4>{props.shoes.title}</h4>
-            <p>{props.shoes.price}</p>
-        </div>
-    );
+  return (
+    <div className="col-md-4">
+      <img
+        src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`}
+        width={"80%"}
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  );
 }
 export default App;
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-const Detail = () => {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <img
-                        src="https://codingapple1.github.io/shop/shoes1.jpg"
-                        width="100%"
-                    />
-                </div>
-                <div className="col-md-6">
-                    <h4 className="pt-5">상품명</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
-                    <button className="btn btn-danger">주문하기</button>
-                </div>
-            </div>
-        </div>
-    );
-};
+let YellowBox = styled.div`
+  background: yellow;
+  color: black;
+  padding: 10px;
+`;
+function Detail() {
+  useEffect(() => {
+    setTimeout(() => {}, 2000);
+  });
+  return (
+    <div>
+      <YellowBox>노랑박스임</YellowBox>;
+    </div>
+  );
+}
+
+// function Detail(props) {
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col-md-6">
+//           <img
+//             src="https://codingapple1.github.io/shop/shoes1.jpg"
+//             width="100%"
+//           />
+//         </div>
+//         <div className="col-md-6 mt-4">
+//           <h4 className="pt-5">{props.shoes[0].title}</h4>
+//           <p>{props.shoes[0].content}</p>
+//           <p>{props.shoes[0].price}원</p>
+//           <button className="btn btn-danger">주문하기</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default Detail;
